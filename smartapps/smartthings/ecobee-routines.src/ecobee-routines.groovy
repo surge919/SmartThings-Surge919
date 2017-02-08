fea081429ce0145c44ea09325c10cfdba921e206
surge919 <surge9one9@gmail.com>
2017-02-08 00:22:18 -0500

    v0.1.6- Logic tweaks, fixed bad state.variableNames
    v0.1.6- Logic tweaks, fixed bad state.variableNames

--- smartapps/smartthings/ecobee-routines.src/ecobee-routines.groovy
+++ smartapps/smartthings/ecobee-routines.src/ecobee-routines.groovy
@@ -1,7 +1,7 @@
 /**
  *  ecobee Routines
  *
- *  Copyright 2015 Sean Kendall Schneyer
+ *  Copyright 2015 Sean Kendall Schneyer 
  *
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
